9a9bee5e389e8a7192052cbc4520ec0e03e48f42
eliteMember <gksa2000@naver.com>
2022-03-03 14:58:30 +0900

    크로스도메인 설정

---
+++ emms_server/src/main/java/com/emms/common/CorsConfiguration.java
@@ -0,0 +1,19 @@
+package com.emms.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer{
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/api/**")
+            .allowedOrigins("http://localhost:3000");
+        	
+        /*	// ▼ 모든 접근에 대하여 허용 할 경우
+        	.allowedOrigins("*");
+       	*/
+    }
+}
