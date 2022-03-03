e41c1d0c10645e2aa075122f1d54fc5d5258c816
parksy1126 <keilman1126@gmail.com>
2022-03-03 10:35:03 +0900

    lombok 디펜전시 추가

--- emms_server/src/main/java/com/emms/api/member/service/TestVO.java
+++ emms_server/src/main/java/com/emms/api/member/service/TestVO.java
@@ -2,6 +2,9 @@ package com.emms.api.member.service;
 
 import java.io.Serializable;
 
+import lombok.Data;
+
+@Data
 public class TestVO implements Serializable {
 
 	
@@ -10,20 +13,6 @@ public class TestVO implements Serializable {
 	 */
 	private static final long serialVersionUID = 6875173717013081459L;
 
-	private Integer no;
-	private String text;
-	public Integer getNo() {
-		return no;
-	}
-	public void setNo(Integer no) {
-		this.no = no;
-	}
-	public String getText() {
-		return text;
-	}
-	public void setText(String text) {
-		this.text = text;
-	}
 
 	
 }
