5f91a5f05d732fa07dc0ff6dd7be646c394dda27
eliteMember <gksa2000@naver.com>
2022-03-16 15:06:40 +0900

    서버 배포를 위해 수정

--- emms_server/src/main/java/com/emms/config/CorsConfiguration.java
+++ emms_server/src/main/java/com/emms/config/CorsConfiguration.java
@@ -10,6 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/api/**")
+            .allowedOrigins("http://192.168.0.6:3000")
             .allowedOrigins("http://localhost:3000");
         	
         /*	// ▼ 모든 접근에 대하여 허용 할 경우
