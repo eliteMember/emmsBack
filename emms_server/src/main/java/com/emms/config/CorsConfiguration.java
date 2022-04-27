dd9242bb1d2dbd1966a9d7e6b04940e494242a17
eliteMember <gksa2000@naver.com>
2022-04-27 16:41:17 +0900

    크로스도메인설정

--- emms_server/src/main/java/com/emms/config/CorsConfiguration.java
+++ emms_server/src/main/java/com/emms/config/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/api/**")
-        	.allowedOrigins("http://localhost:3000","http://192.168.0.6:3000");
+        	.allowedOrigins("http://localhost:3000","http://192.168.0.129:3000");
         	
         /*	// ▼ 모든 접근에 대하여 허용 할 경우
         	.allowedOrigins("*");
